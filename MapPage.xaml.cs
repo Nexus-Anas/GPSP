31e70df357bab6c749ed2ffb1d992a6116952e5a
The-Vigilante <anas@vigilante@gmail.com>
2023-06-14 15:50:13 +0100

    I implemented an algorythm to circle an area of closest pins

--- MapPage.xaml.cs
+++ MapPage.xaml.cs
@@ -42,9 +42,144 @@ public partial class MapPage : ContentView
             };
 
             map.Pins.Add(pin);
+            map.MapClicked += OnMapClicked;
             mapContainer.Content = map;
         }
     }
+
+
+
+
+    private void OnMapClicked(object sender, MapClickedEventArgs e)
+    {
+        var map = (MyMap)sender;
+
+        // Create a new pin at the clicked location
+        var pin = new Pin
+        {
+            Label = "Custom Pin",
+            Type = PinType.Generic,
+            Location = e.Location
+        };
+
+        // Add the pin to the map
+        map.Pins.Add(pin);
+
+        // Check if the new pin is within proximity of other pins
+        var pinsWithinProximity = map.Pins.Where(p => p != pin && CalculateDistance(p.Location, pin.Location) <= 100);
+
+        if (pinsWithinProximity.Count() >= 2)
+        {
+            // Calculate the center point of the pins within proximity
+            var centerLocation = GetCenterLocation(pinsWithinProximity.Select(p => p.Location));
+
+            // Check if the new pin falls within the existing circle
+            var existingCircle = map.MapElements.OfType<Circle>().FirstOrDefault();
+            if (existingCircle != null && CalculateDistance(centerLocation, pin.Location) <= existingCircle.Radius.Meters)
+            {
+                // The new pin falls within the existing circle, no need to create another circle
+                return;
+            }
+
+            // Remove any existing circles from the map
+            map.MapElements.Remove(existingCircle);
+
+            // Create a circle overlay with a radius of 100 meters
+            var circle = new Circle
+            {
+                Center = centerLocation,
+                Radius = new Distance(100),
+                StrokeColor = Colors.Red,
+                StrokeWidth = 2f
+            };
+
+            // Add the circle overlay to the map
+            map.MapElements.Add(circle);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private double CalculateDistance(Location location1, Location location2)
+    {
+        // Calculate the distance between the two locations using the Haversine formula
+        const double earthRadius = 6371000; // Earth's radius in meters
+        double lat1 = location1.Latitude;
+        double lon1 = location1.Longitude;
+        double lat2 = location2.Latitude;
+        double lon2 = location2.Longitude;
+
+        double dLat = DegreesToRadians(lat2 - lat1);
+        double dLon = DegreesToRadians(lon2 - lon1);
+
+        double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                   Math.Cos(DegreesToRadians(lat1)) * Math.Cos(DegreesToRadians(lat2)) *
+                   Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+        double distance = earthRadius * c;
+        return distance;
+    }
+
+    private double DegreesToRadians(double degrees)
+    {
+        return degrees * Math.PI / 180;
+    }
+
+    private Location GetCenterLocation(IEnumerable<Location> locations)
+    {
+        double sumLatitude = 0;
+        double sumLongitude = 0;
+        int count = 0;
+
+        foreach (var location in locations)
+        {
+            sumLatitude += location.Latitude;
+            sumLongitude += location.Longitude;
+            count++;
+        }
+
+        double averageLatitude = sumLatitude / count;
+        double averageLongitude = sumLongitude / count;
+
+        return new Location(averageLatitude, averageLongitude);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     private void btnSearch_Clicked(object sender, EventArgs e) => SearchLocation();
